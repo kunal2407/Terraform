bce09354246a509c09558d4a62dbf07a6141b5c7
kunal07 <kunalkhade@2411.gmail.com>
2024-02-22 12:28:34 +0530

    instance.tf file created

--- instance.tf
+++ instance.tf
@@ -2,11 +2,8 @@ provider "aws" {
     region = "ap-south-1"
 }
 resource "aws_instance" "my_instance" {
-    ami = "ami-0e670eb768a5fc3d4"
+    ami = ""
     instance_type = "t2.micro"
     key_name = "webapp-key"
-    tags = {
-      Name = "new-instance"
-    }
-  
+    security_groups = ""
 }
