208dc10a25b64c743cdc1c9a047112cb8e326e77
kunal2407 <152463980+kunal2407@users.noreply.github.com>
2024-02-26 10:48:17 +0530

    Update main.tf

--- module/main.tf
+++ module/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ap-south-1"
 }
 module "vpc" {
- source  = "./module/vpc"
+ source  = "./vpc"
  vpc_cidr_block     = var.vpc_cidr_block
  public_cidr_block  = var.public_cidr_block
  private_cidr_block = var.private_cidr_block
